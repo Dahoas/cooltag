d1fa5649bdfce179cc933b16d14e01c548cd7242
Dahoas <36314634+Dahoas@users.noreply.github.com>
2020-01-20 21:19:56 -0500

    worked on stuff

--- game/main.js
+++ game/main.js
@@ -7,17 +7,18 @@ let barriers = [];
 let player;
 
 class Player {
-    constructor(x, y) {
+    constructor(x, y, it) {
         this.x = x;
         this.y = y;
+        this.it = it;
     }
 
     update(x, y) {
         this.x = x;
         this.y = y;
+        rect(x, y, itemW, itemW);
     }
     intersects(block) {
-        // from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
         let blockX = block.x * 40;
         let blockY = block.y * 40;
         let shift = 5;
@@ -30,6 +31,7 @@ class Player {
         ) {
             this.x = this.x + shift;
             this.y = this.y + shift;
+            return true;
         } else if (
             blockX <= this.x + itemW &&
             this.x + itemW <= blockX + itemW &&
@@ -38,6 +40,7 @@ class Player {
         ) {
             this.x = this.x - shift;
             this.y = this.y + shift;
+            return true;
         } else if (
             blockX <= this.x &&
             this.x <= blockX + itemW &&
@@ -46,6 +49,7 @@ class Player {
         ) {
             this.x = this.x + shift;
             this.y = this.y - shift;
+            return true;
         } else if (
             blockX <= this.x + itemW &&
             this.x + itemW <= blockX + itemW &&
@@ -54,8 +58,47 @@ class Player {
         ) {
             this.x = this.x - shift;
             this.y = this.y - shift;
+            return true;
+        }
+        else{
+            return false;
         }
     }
+
+    intersect(){
+        let flag = false;
+        for (const barrier of barriers) {
+            for (const block of barrier) {
+                if(this.intersects(block)){
+                    flag = true;
+                }
+            }
+        }
+        return flag;
+    }
+
+    draw(){
+        let vec = createVector(mouseX - this.x, mouseY - this.y);
+        vec.normalize();
+        vec.mult(5);
+        let newX = this.x + vec.x;
+        let newY = this.y + vec.y;
+        if(this.it && !this.intersect()){
+            this.update(newX, newY);
+        }
+        else{
+            this.update(this.x,this.y);
+        }
+    }
+
+    tag(player){
+        let block = {x : player.x/40, y : player.y/40};
+        if(this.it && this.intersects(block)){
+            player.it = true;
+            this.it = false;
+        }
+    }
+
 }
 
 function randomNum(min, max) {
@@ -72,13 +115,16 @@ function offscreen() {
 }
 
 function setup() {
-    player = new Player(40, 40);
+    //Initialize players
+    player1 = new Player(40,40,true);
+    player2 = new Player(1000,500,false);
     canvas = createCanvas(width, height);
     background(50);
     noStroke();
     curX = 50;
     curY = 50;
 
+    //Initialize barriers
     for (let i = 0; i < 8; i++) {
         randomX = randomBlock(0, width);
         randomY = randomBlock(0, height);
@@ -105,30 +151,24 @@ function setup() {
         }
         barriers.push(blocks);
     }
+
 }
 
 function draw() {
     background(50);
     fill(255);
 
-    let vec = createVector(mouseX - player.x, mouseY - player.y);
-    vec.normalize();
-    vec.mult(5);
-
     // draw barriers
     for (const barrier of barriers) {
         for (const block of barrier) {
             rect(block.x * itemW, block.y * itemW, itemW, itemW);
-            player.intersects(block);
         }
     }
 
-    let newX = player.x + vec.x;
-    let newY = player.y + vec.y;
-
-    rect(newX, newY, itemW, itemW);
-
-    player.update(newX, newY);
-
+    //Draw player and calculate movement
+    player1.draw();
+    player2.draw();
+    player1.tag(player2);
+    player2.tag(player1);
     //line(mouseX, 0, mouseX, 100);
 }

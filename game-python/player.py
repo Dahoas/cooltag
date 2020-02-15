f86ddaf1e1c7e34a0ad70b56db96a11b2d417f6b
Christian Broms <cb@christianbroms.com>
2020-02-14 22:22:33 -0500

    push that bad boy

--- game-python/player.py
+++ game-python/player.py
@@ -1,18 +1,47 @@
 class Player:
 
     tagged = False
-    position = {'x': 0, 'y': 0}
-
+    bounce = 0
+    position = [0,0]
+    velocity = [0,0]
+    acceleration = [0,0]
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y 
+        self.position = [x, y]
 
-    def move(self, x, y):
+    def speed(self):
+        return (self.velocity[0]**2 + self.velocity[1]**2)**(1/2)
 
+    def update_pos(self):
+        if (self.tagged):
+            self.velocity = self.velocity + self.acceleration
+            self.position = self.position * self.velocity
 
-        if (not self.moving):
-            # choose new position
-            # magicalML(board, self)
+    def choose_accel(self):
+        self.acceleration = [1,1]
+
+    # def move(self, x, y):
+    #     self.x = x
+    #     self.y = y
+
+    def update(self):
+        self.choose_accel()
+        if (self.tagged and self.intersect() and self.bounce == 0):
+            self.bounce = 30
+            self.acceleration = self.acceleration * -1
+        if (self.bounce == 0):
+            self.velocity = self.velocity + self.acceleration
+            if(self.speed() > 100):
+                self.velocity = (self.velocity/speed())*100
+            # self.update();
+            self.update_pos()
+                        
+        else:
+            self.bounce = self.bounce - 1
+            # this.update();
+            self.update_pos()
+            
+                    
+
       
         

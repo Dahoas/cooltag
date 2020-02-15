e606ed8ccc522e70da1a7bff4f97d692cc481e1b
Dahoas <36314634+Dahoas@users.noreply.github.com>
2020-02-14 22:33:08 -0500

    test

--- game-python/player.py
+++ game-python/player.py
@@ -4,9 +4,10 @@ class Player:
     position = {'x': 0, 'y': 0}
 
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, it):
         self.x = x
         self.y = y 
+        self.it = it
 
     def move(self, x, y):
 
